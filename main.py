78d720092f406c6f633097aee065db50eb8c4e22
박성식 <sosn3@naver.com>
2023-04-17 23:10:24 +0900

    dd

--- main.py
+++ main.py
@@ -3,13 +3,20 @@
 import os
 from typing import Optional
 from fastapi import FastAPI
-from routes.test import router as test_router
+# from routes.test import router as test_router
 
 app = FastAPI() # FastAPI 모듈
-app.include_router(test_router) # 다른 route파일들을 불러와 포함시킴
+# app.include_router(test_router) # 다른 route파일들을 불러와 포함시킴
 
 @app.get("/") # Route Path
 def index():
     return {
         "Python": "Framework-FastAPI",
     }
+    
+@app.get("/list") # Route Path
+def index():
+    pass
+    # return {
+    #     "Python": "Framework-FastAPI",
+    # }
