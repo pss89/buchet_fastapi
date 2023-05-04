a1f9bdb009751d8ddc998b2c53989f9ebbaef236
박성식 <sosn3@naver.com>
2023-05-04 14:10:20 +0900

    routes 폴더 생성 후 클래스화 작업 진행..(main.py 파일 소스코드 정리)

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 # main.py
 
-import os
 from fastapi import FastAPI
 from typing import List,Optional
 from starlette.middleware.cors import CORSMiddleware
-# from routes.test import router as test_router
-# from pydantic import BaseModel
-
-from db.session import session
-from db.models.test_model import TestTable,Test
+from routes.test import router as test_router
 
 app = FastAPI() # FastAPI 모듈
-# app.include_router(test_router) # 다른 route파일들을 불러와 포함시킴
+app.include_router(test_router) # 다른 route파일들을 불러와 포함시킴
 
 app.add_middleware(
     CORSMiddleware,
@@ -26,43 +21,3 @@ def index():
     return {
         "Python": "Framework-FastAPI",
     }
-    
-@app.get("/tests") # Route Path
-def read_tests():
-    tests = session.query(TestTable).all()
-    return tests
-
-@app.get("/tests/{test_id}") # Route Path
-def read_test(test_id:int):
-    test = session.query(TestTable).filter(TestTable.id == test_id).first()
-    return test
-
-@app.post("/tests") # Route Path
-def create_tests(title:str, description:str):
-    test = TestTable()
-    test.title = title
-    test.description = description
-    
-    session.add(test)
-    session.commit()
-    
-    return f"{title} created..."
-
-@app.put("/tests") # Route Path
-def update_tests(tests:List[Test]):
-    
-    for i in tests:
-        test = session.query(TestTable).filter(TestTable.id == i.id).first()
-        test.title = i.title
-        test.description = i.description
-        session.commit()
-        
-        # return f"{i.title} updated" 
-    return f"{test[0].title} update..."
-
-@app.delete("/tests") # Route Path
-def delete_tests(test_id:int):
-    test = session.query(TestTable).filter(TestTable.id == test_id).delete()
-    session.commit()
-    
-    return f"{test_id} delete.."
--- routes/test.py
+++ routes/test.py
@@ -1,19 +1,71 @@
-from typing import Optional
-from fastapi import APIRouter, Depends
-from sqlalchemy.orm import Session
-from db.connection import get_db
-from crud import crud_test
-from apis import test # main logic
+import os
+from dotenv import load_dotenv
+
+from fastapi import APIRouter
+from typing import List,Optional
+from db.session import session
+from db.models.test_model import TestTable,Test
+
+load_dotenv()
 
 router = APIRouter(
-    prefix="/items", # url 앞에 고정적으로 붙는 경로추가
+    # prefix="/items", # url 앞에 고정적으로 붙는 경로추가
 ) # Route 분리
 
-@router.get("/test_route") # Route Path
-def test_index(db: Session = Depends(get_db)):
-	
-	res = test.test_index(db=db) # apis 호출
-	
-	return {
-        "res" : res,
-	} # 결과
+slack_channel_id = os.getenv("SLACK_CHANNEL_ID")
+slack_token = os.getenv("SLACK_TOKEN")
+
+@router.get("/tests") # Route Path
+def read_tests():
+    tests = session.query(TestTable).all()
+    
+    # slack_channel_id = os.getenv("SLACK_CHANNEL_ID")
+    # slack_token = os.getenv("SLACK_TOKEN")
+    
+    return tests
+
+@router.get("/tests/{test_id}") # Route Path
+def read_test(test_id:int):
+    test = session.query(TestTable).filter(TestTable.id == test_id).first()
+    return test
+
+@router.post("/tests") # Route Path
+def create_tests(title:str, description:str):
+    test = TestTable()
+    test.title = title
+    test.description = description
+    
+    session.add(test)
+    session.commit()
+
+	# try:
+	# 	# Call the chat.postMessage method using the WebClient
+	# 	result = client.chat_postMessage(
+	# 		channel=slack_channel_id, 
+	# 		text="Hello world"
+	# 	)
+	# 	logger.info(result)
+
+	# except SlackApiError as e:
+	# 	logger.error(f"Error posting message: {e}")
+    
+    return f"{title} created..."
+
+@router.put("/tests") # Route Path
+def update_tests(tests:List[Test]):
+    
+    for i in tests:
+        test = session.query(TestTable).filter(TestTable.id == i.id).first()
+        test.title = i.title
+        test.description = i.description
+        session.commit()
+        
+        # return f"{i.title} updated" 
+    return f"{test[0].title} update..."
+
+@router.delete("/tests") # Route Path
+def delete_tests(test_id:int):
+    test = session.query(TestTable).filter(TestTable.id == test_id).delete()
+    session.commit()
+    
+    return f"{test_id} delete.."
