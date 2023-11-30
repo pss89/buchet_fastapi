402612d1f6cd285c15e8d4bebaeb246946a06a52
klinelex_sspark <sspark@klinelex.kr>
2023-11-30 10:02:29 +0900

    3-02 게시판 페이징 처리 완료, 3-03 스토어 해야 함

--- frontend/src/routes/Home.svelte
+++ frontend/src/routes/Home.svelte
@@ -4,19 +4,38 @@
 
     let question_list = []
 
-    function get_question_list() {
+    let size = 10
+    let page = 0
+    // 게시물의 총건수
+    let total = 0
+    // 리엑티브 선언 (svelte 에서 상태가 변경될 때 자동으로 업데이트)
+    // 전체 페이지 갯수
+    $: total_page = Math.ceil(total/size)
+
+    function get_question_list(_page) {
         // fetch("http://127.0.0.1:8000/api/question/list").then((response) => {
         //     response.json().then((json) => {
         //         question_list = json
         //     })
         // })
-        fastapi('get', '/api/question/list', {}, (json) => {
-            // question_list = json
+        let params = {
+            page: _page,
+            size: size,
+        }
+
+        fastapi('get', '/api/question/list', params, (json) => {
             question_list = json.question_list
-        })
+            page = _page
+            total = json.total
+        });
+        // fastapi('get', '/api/question/list', {}, (json) => {
+        //     // question_list = json
+        //     question_list = json.question_list
+        // })
     }
 
-    get_question_list()
+    // get_question_list()
+    get_question_list(0)
 </script>
 
 <div class="container my-3">
@@ -40,5 +59,29 @@
         {/each}
         </tbody>
     </table>
+    <!-- 페이징 시작 -->
+    <ul class="pagination justify-content-center">
+        <!-- 이전페이지 -->
+        <!-- 이전 페이지값 없으면 비활성화 -->
+        <li class="page-item {page <= 0 && 'disabled'}">
+            <button class="page-link" on:click="{() => get_question_list(page-1)}">이전</button>
+        </li>
+        <!-- 페이지번호-->
+        {#each Array(total_page) as _, loop_page}
+        <!-- 페이지 제한 -->
+        {#if loop_page >= page-5 && loop_page <= page+5}
+        <!-- 현재 페이지와 가틍면 활성화 -->
+        <li class="page-item {loop_page === page && 'active'}">
+            <button on:click="{() => get_question_list(loop_page)}" class="page-link">{loop_page + 1}</button>
+        </li>
+        {/if}
+        {/each}
+        <!-- 다음 페이지 -->
+        <!-- 다음페이지값 없으면 비활성화 -->
+        <li class="page-item {page >= total_page-1 && 'disabled'}">
+            <button class="page-link" on:click="{() => get_question_list(page+1)}">다음</button>
+        </li>
+    </ul>
+    <!-- 페이징 끝 -->
     <a use:link href="/question-create" class="btn btn-primary">질문 등록하기</a>
 </div>
